f6b82998e0978026bda4f850a8f5d9a33deb658d
Hridhaan33850 <Hridhaan.33850@stu.upes.ac.in>
2026-08-23 22:10:19 +0530

    DAY 14

---
+++ sum of first n odd number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>8
+
+int main() {
+    int n,i,sum=0;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for (i=1; i<=n; i++) {
+        sum=sum+(2*i-1);
+        
+    }
+    printf("Sum of first %d odd natural numbers is %d\n",n,sum);
+    return 0;
+}
